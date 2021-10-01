81a1b55e8762cb44b7cea288ce4d4c95858fce67
Christian Castillo <chriscast94@outlook.com>
2021-09-30 20:49:06 -0700

    fixed storage and time

--- assets/script.js
+++ assets/script.js
@@ -6,45 +6,66 @@
 // Goal: Begin using jquery start below
 
 // Tells the time and date
- var today = moment();
- $("#currentDay").text(today.format("MMMM Do YYYY, h:mm a"));
+var today = moment().format('MMMM Do YYYY, h:mm:ss a');
+$("#currentDay").text(today);
 
 // Ready to go when the page loads
 $(document).ready(function () {
 
-// sets the texts into local Storage
-    $(".save-btn").on("click", function(){
-        var text = $(this).siblings("description").val();
+    // sets the texts into local Storage
+    function saveTask () {
+        var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
 
         localStorage.setItem(time, text);
-    });
+    };
+    $(".saveBtn").on("click", saveTask);
 
-    // Time tracking function
-    function colorTime() {
-
-        var currentTime = moment().hour();
-        $(".time-block").each(function(){
-            var timeBlocks = parseInt($(this).attr("id").split("hour")[1]);
-
-            if (timeBlocks === currentTime) {
-                $(this).addClass("present");
-
-            }
-
-            else if (timeBlocks < currentTime) {
-                $(this).addClass("past");
-                $(this).removeClass("present");
-            }
-
-            else {
-                $(this).addClass("future");
-                $(this).removeClass("present")
-            }
-        });
-    }
-    // Get items from local storage
-colorTime();
 });
+
+// Time tracking function
+function colorTime() {
+
+    var currentTime = moment().hours();
+    $(".time-block").each(function () {
+        var timeBlocks = parseInt($(this).attr("id"));
+        //  console.log(this);
+
+        if (timeBlocks === currentTime) {
+            $(this).addClass("present");
+            $(this).removeClass("past");
+            $(this).removeClass("present")
+            console.log(timeBlocks);
+
+        }
+
+        else if (timeBlocks < currentTime) {
+            $(this).removeClass("present");
+            $(this).removeClass("future");
+            $(this).addClass("past");
+        }
+
+        else {
+            $(this).addClass("future");
+        }
+    });
+}
+
+// Grab items from local storage
+$("#9am .description").val(localStorage.getItem("9am"));
+$("#10am .description").val(localStorage.getItem("10am"));
+$("#11am .description").val(localStorage.getItem("11am"));
+$("#12pm .description").val(localStorage.getItem("12pm"));
+$("#1pm .description").val(localStorage.getItem("1pm"));
+$("#2pm .description").val(localStorage.getItem("2pm"));
+$("#3pm .description").val(localStorage.getItem("3pm"));
+$("#4pm .description").val(localStorage.getItem("4pm"));
+$("#5pm .description").val(localStorage.getItem("5pm"));
+$("#6pm .description").val(localStorage.getItem("6pm"));
+$("#7pm .description").val(localStorage.getItem("7pm"));
+$("#8pm .description").val(localStorage.getItem("8pm"));
+$("#9pm .description").val(localStorage.getItem("9pm"));
+
+colorTime();
 
 
